8856014564909c0c112a7cecf91f17e3928b5a9b
hoon2 <sanghoon.engineer@gmail.com>
2022-10-18 08:22:02 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-// import Focus from "./components/Focus";
-import Focus from "./components/FocusTwo";
 import Timer from "./components/TimerTwo";
-import ScrollIntoView from "./components/ScrollIntoView";
 
 function App() {
-  return <ScrollIntoView />;
+  return <Timer />;
 }
 
 export default App;
